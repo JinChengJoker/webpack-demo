c4dcb2eead6b62bb1a08bb7df81272a6b343d97a
cheng <jinchengjoker@foxmail.com>
2021-09-07 17:19:17 +0800

    use file-loader

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import _ from 'lodash'
 import './style.css'
+import Profile from './profile.jpg'
 
 function component() {
   var element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  var profileImg = new Image();
+  profileImg.src = Profile
+
+  element.appendChild(profileImg)
 
   return element;
 }
