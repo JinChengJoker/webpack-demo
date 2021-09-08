e45a6078873adf20e24ba083ec701411a762890f
cheng <jinchengjoker@foxmail.com>
2021-09-08 18:02:21 +0800

    dynamic import

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
-import _ from 'lodash'
-import './style.css'
-import Profile from './profile.jpg'
-import Data from './data.xml'
-
-function component() {
-  var element = document.createElement('div');
-
+async function getComponent() {
+  const element = document.createElement('div');
+  const {default: _} = await import('lodash')
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  var profileImg = new Image();
-  profileImg.src = Profile
-
-  element.appendChild(profileImg)
-
-  console.log(Data)
-
-  return element;
+  return element
 }
 
-document.body.appendChild(component());
+getComponent().then((component) => {
+  document.body.appendChild(component);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ module.exports = {
   mode: 'development',
   entry: {
     index: './src/index.js',
-    print: './src/print.js',
   },
   output: {
     filename: '[name].bundle.js',
@@ -39,10 +38,5 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: "Webpack"
     })
-  ],
-  optimization: {
-    splitChunks: {
-      chunks: "all"
-    }
-  }
+  ]
 }
